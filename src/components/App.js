be1d0e4ac1115b182d74e9ef39f0b0d35ebbf130
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-01-15 00:06:21 +0530

    develop all initial components of login form application

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
+
+import MainHeader from "./MainHeader/MainHeader"
+import Login from "./Login/Login"
+import Home from "./Home/Home"
 
 const App = () => {
-    return
+    const[isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const loginHandler = (email, password) => {
+        setIsLoggedIn(true)
+    }
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false)
+    }
+
+    return(
+        <Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </Fragment>
+    )
 }
 
 export default App
